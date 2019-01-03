8075442b1316f8112a7e5f4a37be5336fa7164c1
KendrickPC <kennethpchang@gmail.com>
2019-01-03 16:12:31 +0800

    template file structure fixed.

--- Blog/BlogPost/models.py
+++ Blog/BlogPost/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Topic(models.Model):
     """A topic the user is learning about."""
+    # Title definition is a test
+    title = models.CharField(max_length=100)
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
